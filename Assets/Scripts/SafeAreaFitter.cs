fa56abbadb8d121f4506cc12c620198a38bfb504
SON <donghyeonson27@gmail.com>
2026-01-06 10:01:52 +0900

    26.01.06_(3) 공간 Anchoring 기능 구현
    1. 공간 Anchoring 기능 구현을 위한 하이라키 재형성
    
    *하이라키 구조
    AR Session (AR Foundation 제어)
    
    XR Origin (추적용 좌표) (기기 이동 시 위치/회전 변경)
     - AR Camera (AR 카메라)
     - Trackables (AR Foundation의 추척 결과물 데이터 폴더)
     - XR Space (Immersal 지도 정렬 좌표)  (지속 정렬) (생성 오브제는 여기 하위로 둬야함)
       - XR Map (지도 설정)
       - Pins (앵커들)
         - Pin_01...
      - TrackingAnalyzer (정합 카운트, 퀄리티 데이터 포함 오브젝트)
    
    2. 탭(터치) > Pin 생성 > Base Map 좌표(XR Space 좌표) 고정 구현
    탭(2D 화면 좌표) > ARRaycastManager로 3D 맞은 지점 위치 좌표 얻기 > Pin 프리팹 생성 > Pin을 Pins(=XR Space 아래, Immersal 기준 좌표)에 넣기 > 복원
    
    7.URP 14.0.10 > 14.0.12 버전 변경
    Immersal 2.0.4 > 2.0.2 버전 변경
    
    3.Minimum API Level > Android 10.0 (API Level 29)
    Target API Level > Automatic (Highest installed)
    
    4. 생성 앵커 리셋 버튼/기능 생성
    버튼 탭 시 씬 내 핀과 JSON 핀 저장 파일 모두 삭제

---
+++ Assets/Scripts/SafeAreaFitter.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class SafeAreaFitter : MonoBehaviour
+{
+    // 화면 데이터 저장 장소
+    private RectTransform rt;                     
+    private Rect lastSafeArea;                    
+    private ScreenOrientation lastOrientation;    
+
+
+    // 초기 Safe Area 적용
+    private void Awake()
+    {
+        rt = GetComponent<RectTransform>();       
+        Apply();
+    }
+
+    // 변화 감지 > 적용
+    private void Update()
+    {
+        if (Screen.safeArea != lastSafeArea || Screen.orientation != lastOrientation)
+            Apply();
+    }
+
+    // Safe Area 설정
+    private void Apply()
+    {
+        // 기기의 Safe Area 가져오기
+        Rect sa = Screen.safeArea;        // OS에서 제공하는 Safe Area 정보 얻기 위함
+
+        // Safe Area 비교 기준 만들기
+        lastSafeArea = sa;
+        lastOrientation = Screen.orientation;
+
+        // Safe Area 픽셀 좌표 얻기
+        Vector2 anchorMin = sa.position;
+        Vector2 anchorMax = sa.position + sa.size;
+
+        // 앵커 % 값 얻기
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        // Safe Area 설정
+        rt.anchorMin = anchorMin;
+        rt.anchorMax = anchorMax;
+
+        // 유니티에서 자동으로 만드는 오프셋 제거
+        rt.offsetMin = Vector2.zero;
+        rt.offsetMax = Vector2.zero;
+    }
+}
